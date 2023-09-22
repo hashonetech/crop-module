57396ce2655f024c67a3cfe658debf36785c3702
Hashone Tech LLP <hashonetech>
2023-09-22 16:21:41 +0530

    Changelogs: 1. Commons lib version changes

--- cropper/build.gradle.kts
+++ cropper/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.hashone"
-version = "1.0.5"
+version = "1.0.6"
 
 android {
     namespace = "com.hashone.cropper"
@@ -95,7 +95,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.hashone"
                 artifactId = "crop"
-                version = "1.0.5"
+                version = "1.0.6"
 
                 afterEvaluate {
                     from(components["release"])
