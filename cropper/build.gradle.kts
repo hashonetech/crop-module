ab54f594cf142adab2338ffe3a6bd281ac3ce679
Kailash Chavda <kailash.c@hashone.com>
2024-01-02 18:33:59 +0530

    Version updates - v1.2.5
    Changes:
    1. Commons module gradle version - v1.2.11 updated

--- cropper/build.gradle.kts
+++ cropper/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.hashone"
-version = "1.1.4"
+version = "1.1.5"
 
 android {
     namespace = "com.hashone.cropper"
@@ -95,7 +95,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.hashone"
                 artifactId = "crop"
-                version = "1.1.4"
+                version = "1.1.5"
 
                 afterEvaluate {
                     from(components["release"])
